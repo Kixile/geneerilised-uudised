6495aecfdd5331629da760dd15eaa0f719a25d1b
Kixile <Kasutaja@Kasutaja-PC.lan>
2014-03-21 01:06:54 +0200

    Artikli andmete salvestamine andmebaasi pärast artikli loomist

--- src/main/java/com/geneeriliseduudised/servlets/HelloServlet.java
+++ src/main/java/com/geneeriliseduudised/servlets/HelloServlet.java
@@ -2,6 +2,8 @@ package com.geneeriliseduudised.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -11,30 +13,66 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class HelloServlet extends HttpServlet {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	Connection con = null;
+	
+	public void connect(){
+		try {
+			con = DriverManager.getConnection(
+					"jdbc:postgresql://ec2-54-246-101-204.eu-west-1.compute.amazonaws.com:5432/dc09hcdktafoks?user=ahheansgceypsj&password=gVrdggxl82Anv12TSTDqxlrDaG&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
+					);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void submit(String sql){
+		Statement stmt = null;
+		try {
+			stmt = con.createStatement();
+			stmt.executeQuery(sql);
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}	
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
-		
+		connect();
 		String[] head = req.getParameterValues("header");
 		String[] text = req.getParameterValues("text-input");
 		String[] tag = req.getParameterValues("tags-input");
+		DateFormat formaat = new SimpleDateFormat("yyyy-MM-dd");
+		Date today = new Date();
+		String todaystring = formaat.format(today);
+		String userid = "1";
 		
-		Article a = new Article(head[0], text[0], tag[0]);
+		String info = "INSERT INTO artikkel(pealkiri, aeg, sisu, kasutaja_id) "
+				+ "VALUES('" + head[0] + "', '" + todaystring + "', '" + text[0] + "', " + userid + ");";
+		submit(info);
 		
-		Gson gson = new Gson();
-		
-		String json = gson.toJson(a);  
-		
-		PrintWriter writer = new PrintWriter("src/main/webapp/json/"+head[0].replaceAll(" ","_")+".json", "UTF-8");
-		writer.println(json);
-		writer.close();
+//		Article a = new Article(head[0], text[0], tag[0]);
+//		
+//		Gson gson = new Gson();
+//		
+//		String json = gson.toJson(a);  
+//		
+//		PrintWriter writer = new PrintWriter("src/main/webapp/json/"+head[0].replaceAll(" ","_")+".json", "UTF-8");
+//		writer.println(json);
+//		writer.close();
 		
 		resp.sendRedirect("index.html");
 	}
