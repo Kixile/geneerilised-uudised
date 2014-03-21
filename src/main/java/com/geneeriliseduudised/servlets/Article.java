0305fe8c50b5e5180add55fb18cc20d0374d76a9
Kixile <Kasutaja@Kasutaja-PC.lan>
2014-03-21 02:37:32 +0200

    HTML tweaks, database implementation

--- src/main/java/com/geneeriliseduudised/servlets/Article.java
+++ src/main/java/com/geneeriliseduudised/servlets/Article.java
@@ -4,37 +4,81 @@ public class Article {
 	private String header;
 	private String text;
 	private String[] tags;
+	private String date;
+	private String username;
+	private String summary;
 	
-	public Article(String header, String text, String tags) {
+
+	public Article(String header, String text, String tags, String date,
+			String username, String summary) {
 		super();
 		this.header = header;
 		this.text = text;
-		this.tags = tags.replaceAll(" ", "").split(",");
+		this.tags = tags.split(";");
+		this.date = date;
+		this.username = username;
+		this.summary = summary;
 	}
+
 
 	public String getHeader() {
 		return header;
 	}
 
+
 	public void setHeader(String header) {
 		this.header = header;
 	}
+
 
 	public String getText() {
 		return text;
 	}
 
+
 	public void setText(String text) {
 		this.text = text;
 	}
+
 
 	public String[] getTags() {
 		return tags;
 	}
 
+
 	public void setTags(String[] tags) {
 		this.tags = tags;
 	}
-	
-	
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getSummary() {
+		return summary;
+	}
+
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+
 }
--- src/main/java/com/geneeriliseduudised/servlets/ArticleServlet.java
+++ src/main/java/com/geneeriliseduudised/servlets/ArticleServlet.java
@@ -3,6 +3,11 @@ package com.geneeriliseduudised.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -25,38 +30,68 @@ public class ArticleServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private VelocityEngine engine = new VelocityEngine();;
+	Connection con = null;
+	
+	public void connect(){
+		try {
+			con = DriverManager.getConnection(
+					"jdbc:postgresql://ec2-54-246-101-204.eu-west-1.compute.amazonaws.com:5432/dc09hcdktafoks?user=ahheansgceypsj&password=gVrdggxl82Anv12TSTDqxlrDaG&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
+					);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ResultSet request(String sql){
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+//			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}	
+	
 	
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         engine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
         engine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
         engine.init();
-    }
-    private Template getTemplate(HttpServletRequest req) {
-        return engine.getTemplate(req.getRequestURI(), "UTF-8");
-    }
-    
-    private VelocityEngine createTemplateEngine(ServletContext context) {
-        // velocity must know where /src/main/webapp is deployed
-        // details in the developer guide (Configuring resource loaders)
-        String templatePath = context.getRealPath("/");
-
-        VelocityEngine engine = new VelocityEngine();
-        engine.addProperty("file.resource.loader.path", templatePath);
-        engine.init();
-        return engine;
+        connect();
     }
     
     
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
-		
+		String sql = "SELECT * FROM artikkel_create";
+		ResultSet rs = request(sql);
 		Article[] articles = new Article[5];
-		articles[0] = new Article("header333", "texttexttexttexttexttexttexttext", "tag1, tag2");
-		articles[1] = new Article("header2", "texttexttexttexttexttexttexttext", "tag1, tag2");
-		articles[2] = new Article("header3", "texttexttexttexttexttexttexttext", "tag1, tag2");
-		articles[3] = new Article("header4", "texttexttexttexttexttexttexttext", "tag1, tag2");
-		articles[4] = new Article("header5", "texttexttexttexttexttexttexttext", "tag1, tag2");
+		try {
+			int n = 0;
+			while(rs.next() && n < 5){
+				articles[n] = new Article(rs.getString("pealkiri"), 
+						rs.getString("sisu"), "helo", rs.getString("aeg"),
+						rs.getString("kasutajanimi"), rs.getString("lyhisisu"));
+				n++;
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+//		try {
+//			rs.close();
+//		} catch (SQLException e1) {
+//			e1.printStackTrace();
+//		}
+
+//		articles[0] = new Article("header333", "texttexttexttexttexttexttexttext", "tag1, tag2");
+//		articles[1] = new Article("header2", "texttexttexttexttexttexttexttext", "tag1, tag2");
+//		articles[2] = new Article("header3", "texttexttexttexttexttexttexttext", "tag1, tag2");
+//		articles[3] = new Article("header4", "texttexttexttexttexttexttexttext", "tag1, tag2");
+//		articles[4] = new Article("header5", "texttexttexttexttexttexttexttext", "tag1, tag2");
 		
 		
 		VelocityContext context = new VelocityContext();
