6df3b2041945a7fe39f3ca2813536dff56c6b0c4
akustav <akustav@hotmail.com>
2014-05-22 13:48:50 +0300

    Whoops.

---
+++ src/main/java/com/geneeriliseduudised/servlets/AuthorityHandler.java
@@ -0,0 +1,65 @@
+package com.geneeriliseduudised.servlets;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public class AuthorityHandler {
+	private String cookie;
+	private Connection con = null;
+	private ResultSet rs = null;
+	private int email = 0;
+
+	public AuthorityHandler() {
+	}
+	
+	public void connect() {
+		try {
+			con = DriverManager
+					.getConnection("jdbc:postgresql://ec2-54-246-101-204.eu-west-1.compute.amazonaws.com:5432/dc09hcdktafoks?user=ahheansgceypsj&password=gVrdggxl82Anv12TSTDqxlrDaG&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean isLegit(HttpServletRequest req){
+		Statement stmt = null;
+		Cookie[] cookies = req.getCookies();
+		String[] aa = new String[2];
+		for (int i = 0; i < cookies.length; i++) {
+			aa[i] = cookies[i].getValue();
+		}
+		
+		connect();
+		
+		try {
+			String query = "SELECT kasutaja_id FROM sessioonid WHERE sessioon_id = '"+ aa[1] +"';";
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				return true;
+			}
+			stmt.close();
+			rs.close();
+			con.close();
+		} catch (SQLException e3) {
+			e3.printStackTrace();
+			try {
+				stmt.close();
+				rs.close();
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+}
